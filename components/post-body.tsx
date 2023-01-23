44ca218b226b1b26657b5c480f60fd9feb0db224
hysenimursel <hysenimursel@gmail.com>
2023-01-23 22:55:18 +0100

    Update post-body.tsx

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -2,15 +2,14 @@ import styles from './post-body.module.css'
 import adCode from './ad-code.html'; //import the ad code
 
 export default function PostBody({ content }) {
-  let contentString = content.rendered;
-  const paragraphs = contentString.split('</p>');
+  const paragraphs = content.split('</p>');
   paragraphs.splice(2, 0, adCode);
-  contentString = paragraphs.join('</p>');
+  content = paragraphs.join('</p>');
   return (
     <div className="max-w-2xl mx-auto">
       <div
         className={styles.content}
-        dangerouslySetInnerHTML={{ __html: contentString }}
+        dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
   )
