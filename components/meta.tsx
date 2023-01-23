a0b0d8c16b5b1b43f66e22419d9b7b50e0081625
hysenimursel <hysenimursel@gmail.com>
2023-01-23 22:35:33 +0100

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -29,14 +29,7 @@ export default function Meta() {
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   )
 }
