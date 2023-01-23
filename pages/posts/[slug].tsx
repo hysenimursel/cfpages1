105213e9ab6f5f7d1ee6a2f1c12e794033b76eb9
hysenimursel <hysenimursel@gmail.com>
2023-01-23 21:15:20 +0100

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -11,6 +11,7 @@ import SectionSeparator from '../../components/section-separator'
 import Layout from '../../components/layout'
 import PostTitle from '../../components/post-title'
 import Tags from '../../components/tags'
+import HeroPost from '../../components/hero-post'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
 
@@ -33,11 +34,27 @@ export default function Post({ post, posts, preview }) {
             <article>
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} {CMS_NAME}
                 </title>
                 <meta
                   property="og:image"
                   content={post.featuredImage?.node.sourceUrl}
+                />              
+                <meta
+                  property="og:title"
+                  content={post.title}
+                />
+                 <meta
+                  property="og:type"
+                  content="article"
+                />
+                 <meta
+                  property="og:description"
+                  content={post.excerpt}
+                />   
+                <meta
+                  property="og:url"
+                  content={post.slug}
                 />
               </Head>
               <PostHeader
@@ -54,7 +71,6 @@ export default function Post({ post, posts, preview }) {
             </article>
 
             <SectionSeparator />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
           </>
         )}
       </Container>
