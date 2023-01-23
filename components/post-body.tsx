c912d48f9e59bd412a956492b81dd5aa8406250f
hysenimursel <hysenimursel@gmail.com>
2023-01-23 22:45:26 +0100

    Update post-body.tsx

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,6 +1,10 @@
 import styles from './post-body.module.css'
+import adCode from './ad-code.html'; //import the ad code
 
 export default function PostBody({ content }) {
+  const paragraphs = content.split('</p>');
+  paragraphs.splice(2, 0, adCode);
+  content = paragraphs.join('</p>');
   return (
     <div className="max-w-2xl mx-auto">
       <div
