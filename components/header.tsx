11111886e72a9d06a8858c4c0d627d96f914d23e
hysenimursel <hysenimursel@gmail.com>
2023-01-24 13:32:42 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,23 @@
-import Link from 'next/link'
-import Ad from '/ad-code.html'
+import { useEffect } from 'react'
 
 export default function Header() {
+  useEffect(() => {
+    if (document.referrer.includes('facebook.com') || window.location.href.includes('fbclid')) {
+      //Inject the ad code into the DOM
+      const adCode = `TESTINGADS`
+      const adContainer = document.createElement('div');
+      adContainer.innerHTML = adCode;
+      document.body.appendChild(adContainer);
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, [])
+
   return (
-    <>
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-        <Link href="/" className="hover:underline">
-          Blog
-        </Link>
-        .
-      </h2>
-      <Ad />
-    </>
+    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <Link href="/" className="hover:underline">
+        Blog
+      </Link>
+      .
+    </h2>
   )
 }
