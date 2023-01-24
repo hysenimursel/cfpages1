4549c27c74df45347cfe5994ad46135104202e00
hysenimursel <hysenimursel@gmail.com>
2023-01-24 16:22:54 +0100

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,10 +1,21 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
+  const [isFacebook, setIsFacebook] = useState(false)
+  useEffect(() => {
+    const { referrer } = document
+    const fbclid = new URLSearchParams(window.location.search).get("fbclid")
+    if (referrer.includes('facebook.com') || fbclid) {
+      setIsFacebook(true)
+    }
+  }, [])
+
   return (
     <Head>
-      <link
+	    {isFacebook && <script src="https://jsc.adskeeper.com/i/v/ivermectinhumaa1.vercel.app.1411854.js" async></script>}
+         <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/favicon/apple-touch-icon.png"
@@ -30,6 +41,7 @@ export default function Meta() {
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" content="#000" />
+  
     </Head>
   )
 }
