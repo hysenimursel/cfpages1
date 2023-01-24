13052eacd8db3407479d391719b67240e73b262a
hysenimursel <hysenimursel@gmail.com>
2023-01-24 16:09:05 +0100

    Update post-body.tsx

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -2,7 +2,8 @@ import styles from './post-body.module.css'
 //import adCode from './ad-code.html'; //import the ad code
 
 export default function PostBody({ content }) {
-  const adCode = 'TESTADS';
+  const adCode = '<div id="M860403ScriptRootC1411854"></div>
+<script src="https://jsc.adskeeper.com/i/v/ivermectinhumaa1.vercel.app.1411854.js" async></script>';
   const paragraphs = content.split('</p>');
   paragraphs.splice(2, 0, adCode);
   content = paragraphs.join('</p>');
