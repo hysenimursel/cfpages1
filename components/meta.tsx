7607f5c9acb4942f923ae6269b5cc11b4a1ffb67
hysenimursel <hysenimursel@gmail.com>
2023-01-24 09:57:27 +0100

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,3 +34,61 @@ export default function Meta() {
     </Head>
   )
 }
+
+
+
+
+
+
+
+
+
+import Head from 'next/head'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+
+export default function Meta() {
+  useEffect(() => {
+    if (window.location.href.includes('facebook.com')) {
+      // Add ad code after the second paragraph
+      const paragraphs = document.getElementsByTagName('p')
+      if (paragraphs.length >= 2) {
+        const adContainer = document.createElement('div')
+        adContainer.innerHTML = `ADSTESTING`
+        paragraphs[1].after(adContainer)
+      }
+    }
+  }, [])
+
+  return (
+    <Head>
+    <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000" />
+    </Head>
+  )
+}
