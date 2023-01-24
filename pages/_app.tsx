d6fb870f342a71019e8a31a0dab72e88ce10c1d3
hysenimursel <hysenimursel@gmail.com>
2023-01-24 15:44:09 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,15 @@ import { AppProps } from 'next/app'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return
+    <Script id="show-banner">
+  {`<!-- Composite Start -->
+    <div id="M860403ScriptRootC1411835"></div>
+    <script src="https://jsc.adskeeper.com/i/v/ivermectinhumaa1.vercel.app.1411835.js" async></script>
+    <!-- Composite End -->`}
+</Script>
+  <Component {...pageProps} />
 }
 
 export default MyApp
