2363397de2c1fc6b4a71894684b2b207ef6723d7
hysenimursel <hysenimursel@gmail.com>
2023-01-24 15:45:49 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,7 @@ import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  return
-    <Script id="show-banner">
-  {`<!-- Composite Start -->
-    <div id="M860403ScriptRootC1411835"></div>
-    <script src="https://jsc.adskeeper.com/i/v/ivermectinhumaa1.vercel.app.1411835.js" async></script>
-    <!-- Composite End -->`}
-</Script>
-  <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp
