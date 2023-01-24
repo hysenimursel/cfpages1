ff451c3bb7a7617f6e85731cdf03a75267cf1c9f
hysenimursel <hysenimursel@gmail.com>
2023-01-24 13:01:26 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
+import Ad from './ad-code.html'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
-    </h2>
+    <>
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+        <Link href="/" className="hover:underline">
+          Blog
+        </Link>
+        .
+      </h2>
+      <Ad />
+    </>
   )
 }
