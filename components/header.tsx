141d376c4add1303ad960a1fc44e670226fcfc88
hysenimursel <hysenimursel@gmail.com>
2023-01-24 13:56:51 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react'
 import Link from 'next/link'
+import Script from 'next/script'
+
 
 export default function Header() {
  useEffect(() => {
     if (document.referrer.includes('facebook.com') || window.location.href.includes('fbclid')) {
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1495584723679540"
+        onLoad={() => {
+          console.log('Script has loaded')
+        }}
+      />
       //Inject the ad code into the DOM
-    const adCode = `<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1495584723679540" crossorigin="anonymous"></script>
+    const adCode = `<script async="true" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1495584723679540" crossorigin="anonymous"></script>
 <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1495584723679540" data-ad-slot="3765044096" data-ad-format="auto" data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
