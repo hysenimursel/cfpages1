e22c183abacd883f91d34ce6d1c1ddc4cab0600a
hysenimursel <hysenimursel@gmail.com>
2023-08-20 11:40:41 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+export const config = {
+	runtime: 'edge',
+};
 export default function Document() {
   return (
     <Html lang="en">
