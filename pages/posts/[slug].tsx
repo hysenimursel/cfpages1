f3271391f2e7a3e76381d7ef6be4146bffac97f8
hysenimursel <hysenimursel@gmail.com>
2023-08-20 11:42:16 +0200

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -16,6 +16,8 @@ import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
 import Script from 'next/script'
 
+export const runtime = 'edge';
+
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
   const morePosts = posts?.edges
